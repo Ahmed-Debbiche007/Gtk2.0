3fd5d0ee76f762ef0cf5e74e16da2c08d0fae015
dorrarahmouni <dorra.rahmouni@esprit.tn>
2021-12-12 12:38:08 +0100

    foyer

---
+++ gamma/src/callbacks.h
@@ -0,0 +1,64 @@
+#include <gtk/gtk.h>
+
+
+void
+on_button_3_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button5_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_4_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_2_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_1_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_mvt_clicked                  (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_af_alarme_clicked            (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_af_clicked                   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_ajouterd_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button501_clicked                   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_treeview_hebergement_row_activated  (GtkTreeView     *treeview,
+                                        GtkTreePath     *path,
+                                        GtkTreeViewColumn *column,
+                                        gpointer         user_data);
+
+void
+on_button_mod_clicked                  (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_check_id_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_aj_clicked                   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_chercher_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
